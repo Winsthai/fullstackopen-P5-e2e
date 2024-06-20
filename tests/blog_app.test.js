5758a2aad0fef1d0d8c4a9fd0c9c7664efc19462
Winston Thai <winstonthai1@gmail.com>
2024-06-20 17:20:09 -0600

    exercise 5.19 - test that a logged in user can create a blog

--- tests/blog_app.test.js
+++ tests/blog_app.test.js
@@ -39,4 +39,23 @@ describe('Blog app', () => {
         await expect(page.getByText('Matti Luukkainen logged in')).not.toBeVisible()
     })
   })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+        // User logs in
+        await page.getByTestId('username').fill('mluukkai')
+        await page.getByTestId('password').fill('salainen')
+        await page.getByRole('button', { name: 'login' }).click()
+    })
+  
+    test('a new blog can be created', async ({ page }) => {
+        await page.getByRole('button', { name: 'create new blog' }).click()
+        await page.getByPlaceholder('Type title here').fill('New Blog')
+        await page.getByPlaceholder('Type author here').fill('Matti Luukkainen')
+        await page.getByPlaceholder('Type url here').fill('https://www.google.com/')
+        await page.getByRole('button', { name: 'create' }).click()
+
+        await expect(page.getByText('New Blog Matti Luukkainen')).toBeVisible()
+    })
+  })
 })
