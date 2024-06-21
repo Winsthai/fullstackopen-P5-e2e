0a0e5136ce49c5284d21922212f054f6fe275ea3
Winston Thai <winstonthai1@gmail.com>
2024-06-20 18:21:00 -0600

    exercise 5.22 - test that only user who created the blog can see its delete button

--- tests/blog_app.test.js
+++ tests/blog_app.test.js
@@ -1,87 +1,121 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page, request }) => {
-    // Empty out the db
-    await request.post('/api/testing/reset')
-    // Creating a user in the database
-    await request.post('/api/users', {
-        data: {
-          name: 'Matti Luukkainen',
-          username: 'mluukkai',
-          password: 'salainen'
-        }
+    beforeEach(async ({ page, request }) => {
+        // Empty out the db
+        await request.post('/api/testing/reset')
+        // Creating a user in the database
+        await request.post('/api/users', {
+            data: {
+                name: 'Matti Luukkainen',
+                username: 'mluukkai',
+                password: 'salainen'
+            }
+        })
+        await page.goto('/')
     })
-    await page.goto('/')
-  })
 
-  test('Login form is shown', async ({ page }) => {
-    await expect(page.getByText('log in to application')).toBeVisible()
-    await expect(page.getByTestId('username')).toBeVisible()
-    await expect(page.getByTestId('password')).toBeVisible()
-    await expect(page.getByRole('button')).toBeVisible()
-  })
+    test('Login form is shown', async ({ page }) => {
+        await expect(page.getByText('log in to application')).toBeVisible()
+        await expect(page.getByTestId('username')).toBeVisible()
+        await expect(page.getByTestId('password')).toBeVisible()
+        await expect(page.getByRole('button')).toBeVisible()
+    })
 
-  describe('Login', () => {
-    test('succeeds with correct credentials', async ({ page }) => {
-        await page.getByTestId('username').fill('mluukkai')
-        await page.getByTestId('password').fill('salainen')
+    describe('Login', () => {
+        test('succeeds with correct credentials', async ({ page }) => {
+            await page.getByTestId('username').fill('mluukkai')
+            await page.getByTestId('password').fill('salainen')
+            await page.getByRole('button', { name: 'login' }).click()
+
+            await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+        })
+
+        test('fails with wrong credentials', async ({ page }) => {
+            await page.getByTestId('username').fill('mluukkais')
+            await page.getByTestId('password').fill('salainen')
+            await page.getByRole('button', { name: 'login' }).click()
+
+            await expect(page.getByText('Matti Luukkainen logged in')).not.toBeVisible()
+        })
+    })
+
+    describe('When logged in', () => {
+        beforeEach(async ({ page }) => {
+            // User logs in
+            await page.getByTestId('username').fill('mluukkai')
+            await page.getByTestId('password').fill('salainen')
+            await page.getByRole('button', { name: 'login' }).click()
+        })
+
+        test('a new blog can be created', async ({ page }) => {
+            await page.getByRole('button', { name: 'create new blog' }).click()
+            await page.getByPlaceholder('Type title here').fill('New Blog')
+            await page.getByPlaceholder('Type author here').fill('Matti Luukkainen')
+            await page.getByPlaceholder('Type url here').fill('https://www.google.com/')
+            await page.getByRole('button', { name: 'create' }).click()
+
+            await expect(page.getByText('New Blog Matti Luukkainen')).toBeVisible()
+        })
+
+        describe('and a blog is already created by the logged in user', () => {
+            beforeEach(async ({ page }) => {
+                // User Matti Luukkainen creates a blog
+                await page.getByRole('button', { name: 'create new blog' }).click()
+                await page.getByPlaceholder('Type title here').fill('New Blog')
+                await page.getByPlaceholder('Type author here').fill('Matti Luukkainen')
+                await page.getByPlaceholder('Type url here').fill('https://www.google.com/')
+                await page.getByRole('button', { name: 'create' }).click()
+            })
+
+            test('the blog can be liked', async ({ page }) => {
+                await page.getByRole('button', { name: 'view' }).click()
+                await expect(page.getByText('likes: 0')).toBeVisible()
+                await page.getByRole('button', { name: 'like' }).click()
+                await expect(page.getByText('likes: 1')).toBeVisible()
+            })
+
+            test('that the user who created the blog can delete it', async ({ page }) => {
+                await page.getByRole('button', { name: 'view' }).click()
+                page.on('dialog', dialog => dialog.accept());
+                await page.getByRole('button', { name: 'remove' }).click()
+
+                await expect(page.getByText('New Blog Matti Luukkainen')).not.toBeVisible()
+            })
+        })
+    })
+
+    test('That a user who did not create a blog cannot see the button to delete it', async ({ page, request }) => {
+        // Creating a separate user in the database
+        await request.post('/api/users', {
+            data: {
+                name: 'Winston Thai',
+                username: 'winsthai',
+                password: 'passwordtesting1234'
+            }
+        })
+
+        // Logging in with the new user
+        await page.getByTestId('username').fill('winsthai')
+        await page.getByTestId('password').fill('passwordtesting1234')
         await page.getByRole('button', { name: 'login' }).click()
 
-        await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
-    })
-
-    test('fails with wrong credentials', async ({ page }) => {
-        await page.getByTestId('username').fill('mluukkais')
-        await page.getByTestId('password').fill('salainen')
-        await page.getByRole('button', { name: 'login' }).click()
-
-        await expect(page.getByText('Matti Luukkainen logged in')).not.toBeVisible()
-    })
-  })
-
-  describe('When logged in', () => {
-    beforeEach(async ({ page }) => {
-        // User logs in
-        await page.getByTestId('username').fill('mluukkai')
-        await page.getByTestId('password').fill('salainen')
-        await page.getByRole('button', { name: 'login' }).click()
-    })
-  
-    test('a new blog can be created', async ({ page }) => {
+        // Creating a new blog
         await page.getByRole('button', { name: 'create new blog' }).click()
         await page.getByPlaceholder('Type title here').fill('New Blog')
         await page.getByPlaceholder('Type author here').fill('Matti Luukkainen')
         await page.getByPlaceholder('Type url here').fill('https://www.google.com/')
         await page.getByRole('button', { name: 'create' }).click()
 
-        await expect(page.getByText('New Blog Matti Luukkainen')).toBeVisible()
-    })
+        // Logging out
+        await page.getByRole('button', { name: 'logout' }).click()
 
-    describe('and a blog is already created', () => {
-        beforeEach(async ({ page }) => {
-            // User Matti Luukkainen creates a blog
-            await page.getByRole('button', { name: 'create new blog' }).click()
-            await page.getByPlaceholder('Type title here').fill('New Blog')
-            await page.getByPlaceholder('Type author here').fill('Matti Luukkainen')
-            await page.getByPlaceholder('Type url here').fill('https://www.google.com/')
-            await page.getByRole('button', { name: 'create' }).click()
-        })
+        // Logging in with different user and ensuring delete button is not present
+        await page.getByTestId('username').fill('mluukkai')
+        await page.getByTestId('password').fill('salainen')
+        await page.getByRole('button', { name: 'login' }).click()
+        await page.getByRole('button', { name: 'view' }).click()
 
-        test('the blog can be liked', async ({ page }) => {
-            await page.getByRole('button', { name: 'view' }).click()
-            await expect(page.getByText('likes: 0')).toBeVisible()
-            await page.getByRole('button', { name: 'like' }).click()  
-            await expect(page.getByText('likes: 1')).toBeVisible()
-        })
-
-        test('that the user who created the blog can delete it', async ({ page }) => {
-            await page.getByRole('button', { name: 'view' }).click()
-            page.on('dialog', dialog => dialog.accept());
-            await page.getByRole('button', { name: 'remove' }).click()
-
-            await expect(page.getByText('New Blog Matti Luukkainen')).not.toBeVisible()
-        })
-    })
-  })
+        await expect(page.getByRole('button', { name: 'remove' })).not.toBeVisible()
+    }) 
 })
